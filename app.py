cd5b9092a17d84aa7b1fc682547cb18772504892
Alexander Houston <howdy140@gmail.com>
2018-06-27 21:43:31 -0700

    api done

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, abort
+import json
+
+app = Flask(__name__)
+
+print __name__
+
+@app.route('/v1/api/hello_world')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/v1/api/hello_world/json')
+def hello_json_world():
+    return json.dumps({'message': hello_world()})
+
+@app.route('/v1/api/math/square/<NUM1>', methods=['POST'])
+def square_num(NUM1):
+    try:
+        product = float(NUM1) ** 2.0
+    except ValueError:
+        abort(400)
+    return json.dumps({'expression': '{} * {}'.format(NUM1, NUM1), 'product': product})
+
+@app.route('/v1/api/math/sqrt/<NUM1>', methods=['POST'])
+def sqrt_num(NUM1):
+    try:
+        root = float(NUM1)**(1/2.0)
+    except ValueError:
+        abort(400)
+    return json.dumps({'expression': 'sqrt({})'.format(NUM1), 'product': root})
+
+
--- create_vpc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-gcloud -v >/dev/null || exit
-
-VPC_NAME="myprivatenetwork"
-
-gcloud compute networks create $VPC_NAME
