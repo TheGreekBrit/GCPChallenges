191c64066d1d885ebbf47defa63c29a18a921406
Alexander Houston <howdy140@gmail.com>
2018-07-25 17:20:00 -0700

    service accounts

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,13 @@
 
 SERVICE_ACCOUNT="challenge-user"
 PROJECT_ID="intrepid-craft-195800"
+SERVICE_ACCOUNT_EMAIL="$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com"
 
 gcloud iam service-accounts create $SERVICE_ACCOUNT
+
+gcloud iam service-accounts add-iam-policy-binding $SERVICE_ACCOUNT_EMAIL \
+	--member="serviceAccount:$SERVICE_ACCOUNT_EMAIL" \
+	--role='roles/owner'
 
 gcloud iam service-accounts keys create client_secrets.json \
 	--iam-account=$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com
