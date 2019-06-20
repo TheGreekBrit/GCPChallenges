a5f5c4f59f745dfeacc031f27c32853d0d0085d3
Alexander <howdy140@gmail.com>
2019-06-20 01:21:22 -0700

    automation!

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 terraform apply -auto-approve deploy/app		# deploy app to gce
 mv terraform.tfstate{,.tmp} 	# ignore some resouces for a minute
 gcloud compute instances stop flask-instance #&& gcloud compute images create flask-image --source-disk flask-instance
-terraform apply deploy/infra 	# create image, deploy MIG/LB/etc
+terraform apply -auto-approve deploy/infra 	# create image, deploy MIG/LB/etc
 mv terraform.tfstate{.tmp,}
